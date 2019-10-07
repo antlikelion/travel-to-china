b6e3a871224c257403dfb21c9d075411897f0e54
antlikelion <ignorant_woo@naver.com>
2019-10-08 08:42:48 +0900

    깃 초기화

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Railway from "./components/Railway";
+import "./components/css/App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="whole">
+      <Railway />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Railway.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./css/Railway.css";
+
+const Railway: React.FC = () => {
+  return <div className="Railway_body">기찻길</div>;
+};
+
+export default Railway;
